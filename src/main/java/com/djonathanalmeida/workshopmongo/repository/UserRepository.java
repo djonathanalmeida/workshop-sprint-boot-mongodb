f002fce00983700b27e598e85fda256c775e1a93
Djonathan Almeida <djonathan_7@hotmail.com>
2023-11-09 15:11:00 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/djonathanalmeida/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.djonathanalmeida.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.djonathanalmeida.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+
+}
