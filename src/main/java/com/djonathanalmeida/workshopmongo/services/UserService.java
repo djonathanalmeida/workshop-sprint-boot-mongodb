a697f5389b8cff33f00709f4e6ad80e8b5bbc460
Djonathan Almeida <djonathan_7@hotmail.com>
2023-11-13 19:05:01 -0300

    Obtendo um usuário por id

--- src/main/java/com/djonathanalmeida/workshopmongo/services/UserService.java
+++ src/main/java/com/djonathanalmeida/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.djonathanalmeida.workshopmongo.domain.User;
 import com.djonathanalmeida.workshopmongo.repository.UserRepository;
+import com.djonathanalmeida.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,14 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id){
+		User user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 	
 }
